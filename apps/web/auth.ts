9e9adafb32c1b16f187f8ce74435ecad18fd7d82
Zeeshan Khan <11982876+dropocol@users.noreply.github.com>
2024-07-28 00:48:25 +0500

    no message

---
+++ apps/web/auth.ts
@@ -0,0 +1,81 @@
+import NextAuth from "next-auth";
+// import GitHub from "next-auth/providers/github";
+// import Google from "next-auth/providers/google";
+import { authOptions } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+
+export const { auth, handlers, signIn, signOut } = NextAuth(authOptions);
+
+export async function getSession() {
+  return auth() as Promise<{
+    user: {
+      id: string;
+      name: string;
+      username: string;
+      email: string;
+      image: string;
+    };
+  } | null>;
+  //   const session = await auth();
+  //   console.log({ session });
+  //   return {
+  //     ...session,
+  //   };
+}
+
+export function withSiteAuth(action: any) {
+  return async (
+    formData: FormData | null,
+    siteId: string,
+    key: string | null,
+  ) => {
+    const session = await getSession();
+    if (!session) {
+      return {
+        error: "Not authenticated",
+      };
+    }
+    const site = await prisma.site.findUnique({
+      where: {
+        id: siteId,
+      },
+    });
+    if (!site || !session.user || site.userId !== session.user.id) {
+      return {
+        error: "Not authorized",
+      };
+    }
+
+    return action(formData, site, key);
+  };
+}
+
+export async function withPostAuth(action: any) {
+  return async (
+    formData: FormData | null,
+    postId: string,
+    key: string | null,
+  ) => {
+    const session = await getSession();
+    if (!session || !session.user?.id) {
+      return {
+        error: "Not authenticated",
+      };
+    }
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        site: true,
+      },
+    });
+    if (!post || post.userId !== session.user.id) {
+      return {
+        error: "Post not found",
+      };
+    }
+
+    return action(formData, post, key);
+  };
+}
