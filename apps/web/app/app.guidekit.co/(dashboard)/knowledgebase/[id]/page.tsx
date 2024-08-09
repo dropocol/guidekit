2ac525586e8823f842054fcf74faefee4a17e64b
Zeeshan Khan <11982876+dropocol@users.noreply.github.com>
2024-08-09 15:16:22 +0500

    WIP

--- apps/web/app/app.guidekit.co/(dashboard)/knowledgebase/[id]/page.tsx
+++ apps/web/app/app.guidekit.co/(dashboard)/knowledgebase/[id]/page.tsx
@@ -13,16 +13,13 @@ export default async function KnowledgebasePage({
     redirect("/login");
   }
 
-  const knowledgebase = await prisma.knowledgebase.findUnique({
+  const knowledgebaseCollections = await prisma.collection.findMany({
     where: {
-      id: params.id,
-    },
-    include: {
-      // articles: true,
+      knowledgebaseId: params.id,
     },
   });
 
-  if (!knowledgebase || knowledgebase.userId !== session.user?.id) {
+  if (!knowledgebaseCollections) {
     notFound();
   }
 
@@ -30,18 +27,18 @@ export default async function KnowledgebasePage({
     <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
       <div className="flex flex-col space-y-6">
         <h1 className="font-cal text-3xl font-bold dark:text-white">
-          {knowledgebase.name}
+          {knowledgebaseCollections.length} Collections
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {/* {knowledgebase.articles.map((article) => (
+          {knowledgebaseCollections.map((collection) => (
             <Link
-              key={article.id}
-              href={`/article/${article.id}`}
+              key={collection.id}
+              href={`/article/${collection.id}`}
               className="rounded-lg border border-stone-200 p-4 transition-all hover:bg-stone-100 dark:border-stone-700 dark:hover:bg-stone-800"
             >
-              <h2 className="font-semibold">{article.title}</h2>
+              <h2 className="font-semibold">{collection.name}</h2>
             </Link>
-          ))} */}
+          ))}
         </div>
       </div>
     </div>
