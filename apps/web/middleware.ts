03ce5e9d85d3142ef59580039bb24168b1125e3b
Zeeshan Khan <11982876+dropocol@users.noreply.github.com>
2024-09-19 13:55:03 +0500

    no message

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,5 +1,8 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 import { auth } from "@/auth";
+// import type { NextRequest } from "next/server";
+
+import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 interface CustomNextRequest extends NextRequest {
